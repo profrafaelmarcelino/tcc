29378bce9646f24270779704606e2540c9e64561
profrafaelmarcelino <167098843+profrafaelmarcelino@users.noreply.github.com>
2024-05-16 08:41:42 -0300

    Add files via upload

---
+++ logar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>..: Hotel Escola - ETEC DE ARAÇATUBA</title>
+</head>
+<body>
+<?php
+$login = $_POST['login'];
+$senha = $_POST['senha'];
+echo "O login informado é:$login";
+echo "<p>";
+echo "A senha informada é:$senha";
+
+
+?>
+</body>
+</html>
